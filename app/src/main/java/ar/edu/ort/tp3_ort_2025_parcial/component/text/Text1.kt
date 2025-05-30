deff99000b1c20a9609e270a9f52f2ff9d31da25
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2025-05-30 19:51:00 -0300

    - Update Welcome Screen with new design and Carousel - Refactor Button1 component for better reusability and apply new styles - Adjust Typography for Title and Body text - Add Carousel component with Pager and indicator dots - Define CarouselItem data class and provide items for the Carousel - Add new drawable resources for Carousel images and indicator dots - Add new string resources for Welcome Screen title, button, and Carousel text - Add `androidx.foundation` dependency

--- app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/text/Text1.kt
+++ app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/text/Text1.kt
@@ -3,12 +3,18 @@ package ar.edu.ort.tp3_ort_2025_parcial.component.text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 @Composable
-fun Text1(text: String) {
+fun Text1(
+    text: String,
+    modifier: Modifier = Modifier
+) {
     Text(
         text = text,
-        style = MaterialTheme.typography.bodyMedium
+        style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.onBackground,
+        modifier = modifier,
     )
 }
