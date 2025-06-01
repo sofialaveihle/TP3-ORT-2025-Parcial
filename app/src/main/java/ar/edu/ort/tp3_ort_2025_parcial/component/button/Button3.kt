355f2aefee6628804f6236751f78b8d411eb1f9e
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2025-06-01 20:37:01 -0300

    Refactor: Simplify Button3 composable
    The `Button3` composable has been refactored to remove an unused import of `LocalContentColor`. This change streamlines the component and removes unnecessary dependencies.

--- app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/button/Button3.kt
+++ app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/button/Button3.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
