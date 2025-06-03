795fb85beed1560b6efc7e367400eec5586a2d93
Sofia Laveihle <sofialaveihle@gmail.com>
2025-06-02 21:03:25 -0300

    Increase ButtonSocialMediaLogin icon size

--- app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/button/ButtonSocialMediaLogin.kt
+++ app/src/main/java/ar/edu/ort/tp3_ort_2025_parcial/component/button/ButtonSocialMediaLogin.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,7 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ar.edu.ort.tp3_ort_2025_parcial.R
-import ar.edu.ort.tp3_ort_2025_parcial.ui.theme.Blue
 import ar.edu.ort.tp3_ort_2025_parcial.ui.theme.Gray5
 import ar.edu.ort.tp3_ort_2025_parcial.ui.theme.White
 
@@ -76,7 +72,7 @@ fun ButtonSocialMediaLogin(
             Image(
                 painter = icon2,
                 contentDescription = contentDescription2,
-                modifier = Modifier.size(screenHeight * 0.06f)
+                modifier = Modifier.size(screenHeight * 0.08f)
             )
         }
     }
